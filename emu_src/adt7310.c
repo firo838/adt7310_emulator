1c28c54fbdade68d58b4752991cd8486465c8b23
rosev <rosev@rosev-mbp.local>
2018-11-02 19:36:30 +0900

    fix type.

--- emu_src/adt7310.c
+++ emu_src/adt7310.c
@@ -121,8 +121,7 @@ gen_temp2(adt7310_t *handle)
 
 float
 random_temp(){
-    //double t = 27.125;
-    double a, t;
+    float a, t;
     srand((unsigned int)time(NULL));
 	t = (rand()%10000)/1000 + 15.0;
 
