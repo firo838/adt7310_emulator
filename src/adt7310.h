68c60406613d36cac65b725e2546bd29960e4dfb
rosev <9513480+rosev838@users.noreply.github.com>
2018-12-03 11:26:50 +0900

    fix header commnet.

--- src/adt7310.h
+++ src/adt7310.h
@@ -2,7 +2,7 @@
 * ADT7310 emulator.
 *
 * MIT Lisence
-* Copyright <2018> < rosev : https://rosev838.github.io >
+* Copyright 2018 rosev : https://rosev838.github.io
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
 * software and associated documentation files (the "Software"), to deal in the Software 
 * without restriction, including without limitation the rights to use, copy, modify, 
